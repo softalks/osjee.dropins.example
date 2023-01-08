09c4feed352ce3cd7380ffd6814bd4eba236f86d
Santiago Ruiz <hiebra@hotmail.com>
2023-01-08 16:08:15 +0100

    Revemo \r from line deliminters

--- loop.sh
+++ loop.sh
@@ -1,7 +1,7 @@
 #/bin/bash
-count=0
+requests=0
 url=http://localhost:9080/$1/hello
 while :
 do
-  echo -ne "request #$((count=$count+1)): $(curl --silent --show-error --fail $url)\033[0K\r"
+  echo -ne "request #$((requests=$requests+1)): $(curl --silent --show-error --fail $url)\033[0K\r"
 done
